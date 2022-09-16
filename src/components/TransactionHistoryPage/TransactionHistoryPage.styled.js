ced3be54c97d638baaf0d0fa338c276d558c4799
Denis <fox.ruden@gmail.com>
2022-09-16 17:53:05 +0300

    save

---
+++ src/components/TransactionHistoryPage/TransactionHistoryPage.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  background-color: lightgray;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  gap: 8px;
+`;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const DateContainer = styled.p`
+  display: flex;
+  gap: 8px;
+  margin-bottom: 4px;
+`;
+export const Summary = styled.p`
+  margin-bottom: 4px;
+`;
+export const Currency = styled.p``;
