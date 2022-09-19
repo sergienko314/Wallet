c954665d28a05af24e62da5085b6b15baf478cc1
user name <106239754+Anatoliy-Bolyukh@users.noreply.github.com>
2022-09-19 16:03:27 +0300

    add localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,32 @@ import MainPage from './MainPage';
 import TransactionHistoryPage from './TransactionHistoryPage';
 
 export class App extends Component {
-  state = { activePage: 'main', deduction: [], income: [] };
+  state = {
+    activePage: 'main',
+    deduction: [],
+    income: [],
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.deduction !== this.state.deduction) {
+      localStorage.setItem('deduction', JSON.stringify(this.state.deduction))
+    }
+
+        if (prevState.income !== this.state.income) {
+      localStorage.setItem('income', JSON.stringify(this.state.income))
+    }
+  }
+
+  componentDidMount() {
+    const deduction = JSON.parse(localStorage.getItem('deduction'))
+    const income = JSON.parse(localStorage.getItem('income'))
+    this.setState({
+    deduction,
+    income,
+    })
+  }
+
+
 
   changePageHandler = (page = 'main') => {
     this.setState({ activePage: page });
